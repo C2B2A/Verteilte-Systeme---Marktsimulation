5fb1f5fa017443dcda8ecaa6af642015f2b5b920
C. B.A. <.>
2025-06-10 11:37:05 +0200

    Initial text message

--- main/main.java
+++ main/main.java
@@ -1,6 +1,8 @@
 package main;
 public class main {
     public static void main(String[] args){
+        System.out.println("\n Hello User!");
+        System.out.println("This is a market simulation.");
         
     }
 }
